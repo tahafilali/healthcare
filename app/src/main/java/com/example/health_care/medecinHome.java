4da183a08cfafd396d5a54f2b9a1545a5529ec9b
Taha <tahafilali27@gmail.com>
2020-05-06 22:18:53 +0200

    first commit

--- app/src/main/java/com/example/health_care/medecinHome.java
+++ app/src/main/java/com/example/health_care/medecinHome.java
@@ -8,21 +8,30 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class medecinHome extends AppCompatActivity {
 LinearLayout patients;
+CardView invit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medecin_home);
+        setContentView(R.layout.content_medecin_home);
         patients = findViewById(R.id.list_pats);
+        invit = findViewById(R.id.invit);
         patients.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(medecinHome.this, listePatients.class));
+            }
+        });
+        invit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(medecinHome.this,ListeInvitations.class));
             }
         });
 
