1ec020205e34d9d4c32c95a29dd4fb77324c04c1
Taha <tahafilali27@gmail.com>
2020-05-07 14:22:14 +0200

    first commit

--- app/src/main/java/com/example/health_care/models/RequestViewHolder.java
+++ app/src/main/java/com/example/health_care/models/RequestViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.health_care.models;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.health_care.R;
@@ -9,9 +10,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class RequestViewHolder extends RecyclerView.ViewHolder{
     public TextView name;
-    public RequestViewHolder(View itemView){
+    public Button btn1;
+    public Button btn2;
+    public TextView names;
+    public Patient patient;
+
+    public RequestViewHolder(View itemView,View itemView1){
         super(itemView);
         name = itemView.findViewById(R.id.name);
-
+        names = itemView1.findViewById(R.id.names);
+        btn1 = itemView1.findViewById(R.id.send);
+        btn2 = itemView1.findViewById(R.id.cancel);
     }
 }
